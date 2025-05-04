bc9404cc70d2057075e94ae690fc4f69d96c68f7
laptop acer <amarr140306@gmail.com>
2025-05-04 18:44:44 +0800

    pertemuan ke 8

---
+++ index.js
@@ -0,0 +1,60 @@
+
+// let angka = '1'
+
+// console.log (angka);
+
+//variabel naming
+//mulai dengan huruf,garis bawah dan tanda dolar $
+
+//kondisi hanya bisa membaca boolean 
+
+// let result = 10 > 5 || 5 < 10 || 5 == 10
+
+// console.log (result)
+
+// for (let i = 0 ; i <= 15; i++ ){
+//     if( i % 3 == 0 && i%5 ==0 ) console.log('fizz buzz ')
+//     else if (i%5 == 0) console.log('buzz')
+//     else if (i%3 == 0) console.log('fizz')
+//     console.log(i);
+// }
+//pertemuan 2
+
+
+// for (let i = 1; i <= 100; i++){
+//     console.log("ini adalah angka ke : " + (i));
+// }
+
+// let i = 0;
+// while (i < 5){
+//     console.log ("ini iterasi ke - " + (i + 1))
+//     i++;
+// }
+
+// function sayhello(){
+//     console.log ("halo ges")
+// }
+
+// sayhello()
+
+//   => (arrow function untuk meringkas/memudahkan)
+
+// const func =  (a,b) => a + b;
+// console.log (func(1,2))
+
+
+// let action = "anjayyyy sleebewww";
+// console.log (action.length);
+
+// console.log (Math.random())
+
+// console.log (("b" +  + 'a' + "a" ))
+
+let mobil = {
+    warna : "pink",
+    tipe : "toyota",
+}
+
+console.log(mobil.tipe)
+
+
